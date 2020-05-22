c2e279f0db9d1bbfbd5c8cba1428d20d3e1f9cfb
liwuxi1633 <liwuxi1633@163.com>
2020-05-22 14:24:33 +0800

    数量

--- src/main/java/com/wuxi/trans/JsonFileToExcel.java
+++ src/main/java/com/wuxi/trans/JsonFileToExcel.java
@@ -91,8 +91,6 @@ public final class JsonFileToExcel {
             return;
         }
 
-
-
         final JsonParser parser = new JsonParser();
         List<JsonObject> jsonObjectList = jsonList.stream().map(e -> parser.parse(e).getAsJsonObject()).collect(Collectors.toList());
         writeExcel(jsonObjectList, targetPath, timeField, rowTitle);
